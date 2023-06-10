634008c829cea2d080a9325060694f0242c62599
mateusinacioo <mateus.inacio321@gmail.com>
2023-06-10 10:30:55 -0300

    Aplicando layout do site

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -2,7 +2,7 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   before_action :verify_password, only: [:update]
   before_action :set_admin, only: [:edit, :update, :destroy]
   def index
-      @admins = Admin.all.page(params[:page])
+    @admins = Admin.all.page(params[:page])
   end
 
   def new 
