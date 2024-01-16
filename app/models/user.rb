4ccebf5b585d5c68996b03ba504ad10d0887078c
mateusinacioo <mateus.inacio321@gmail.com>
2024-01-16 06:43:59 -0300

    Validação no campo first name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  # Validatios
+  validates :first_name, presence: true, length: {minimum:3}, on: :update
 
+
+
+         # Virtual Atributes
   def full_name 
     [self.first_name,self.last_name].join(' ')
   end
